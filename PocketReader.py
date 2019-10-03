fcd91799abce9598f2c13b2f9c0edb1ae0365c9f
了不起的菠萝君 <592154873@qq.com>
2019-10-03 22:16:04 +0800

    Update PocketReader.py
    1、UA修改为微信UA，防止翻车，
    2、解决上一个版本调用Server酱接口系统消息错误的消息ID问题
    3、提醒时间优化，增加周几显示

--- PocketReader.py
+++ PocketReader.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     def TencentReader():
         url = "https://kdy.unisyou.net/yunpan/activity/doSign"
         head = {}
-        head['User-Agent'] = 'Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1'
+        head['User-Agent'] = 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_1_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/16D57 MicroMessenger/7.0.3(0x17000321) NetType/WIFI Language/zh_CN'
         head['Accept'] = "application/json, text/javascript, */*; q=0.01"
         head["Content-Type"] = "application/json"
         head["DNT"] = "1"
@@ -21,7 +21,7 @@ if __name__ == "__main__":
 		#自己的Request payload
         req = requests.post(url, data=json.dumps(
             {'userName': "自己注册的手机号", 'deviceNumber': "设备号", 'activityId': 'id号',
-             'mobile': "iOSNaN --iPad"}), headers=head)
+             'mobile': "iOS11 --iPhone"}), headers=head)
         postmsg = req.content.decode("utf-8")
         print(postmsg)
         translate_results = json.loads(postmsg)
@@ -29,18 +29,22 @@ if __name__ == "__main__":
         return translate_results['resultData']
 
     def SendWeChat():
-        localtime = u"日期：" + time.strftime("%Y-%m-%d", time.localtime()) + u"时间：" + time.strftime("%H:%M:%S", time.localtime())
-        print(u"本地时间为 :", localtime)
+        Week = ['周日','周一','周二','周三','周四','周五','周六']
+        localtime = time.strftime("%m", time.localtime()) + u"月" + time.strftime("%d", time.localtime()) + u"日"\
+                    + Week[int(time.strftime("%w", time.localtime()))] + time.strftime("%H", time.localtime()) + u'时' + time.strftime("%M", time.localtime()) + u'分'
+        print(u"Debug：\n本地时间为 :", localtime)
         #自己的Server酱key
-        api = ""
-        title = localtime + u"【口袋阅签到提醒】"
-        content = u""""""
-        data = {
-            "text": title,
-            "desp": content
+        api = "SCU2141T2aa93a5d22cd093b32882b6e53beb38c57dd351715323"
+        title =  u"口袋阅签到提醒_" + localtime
+        content = ""
+        payload = {
+            'text': title,
+            'desp': content
         }
-        print(data)
-        req = requests.post(api, data=data)
+        print(payload)
+        url = 'https://sc.ftqq.com/{}.send'.format(api)
+        requests.post(url, params=payload)
     if(TencentReader() is None):
         print(u'已发送签到提醒')
         SendWeChat()
+
